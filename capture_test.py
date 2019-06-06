38ff99545f64c05937b0440d9d3419667dcc5797
kwhtsng <2018m002@onomichi-u.ac.jp>
2019-06-06 23:48:57 +0900

    delete images

--- capture_test.py
+++ capture_test.py
@@ -1,8 +1,56 @@
-﻿import cv2
+# -*- coding:utf-8 -*-
+
+import cv2
 import os
-# import datetime
+import tkinter as tk
+import sys
+
+# 座標を入力するためのウィンドウを表示する関数
+def input_form():
+
+    # 確定ボタンが押された時のコールバック関数
+    def coord_get():
+
+        # Entryウィジェットのテキストを読み取るgetメソッド
+        global coord
+        coord = txt.get()
+
+        if not coord:
+            # 空欄だったら処理しない
+            pass
+
+        root.destroy()
+
+    # ウィンドウの定義
+    root = tk.Tk()
+    root.title(u"Coordinate input")
+    root.geometry("300x300")
+
+    # フレームの定義
+    frame =tk.Frame(root, pady=10)
+    frame.pack()
+
+    # ラベル
+    label = tk.Label(frame, font=("",14), text="座標->")
+    label.pack(side="left")
+
+    # テキストボックス
+    txt = tk.Entry(frame, font=("",14), justify="center", width=10)
+    txt.focus()
+    txt.pack(side="left")
+
+    # エンターキーで入力
+    root.bind("<Return>", coord_get)
+
+    # ボタンでも入力できるように
+    button = tk.Button(root, text="確定", font=("",15), width=10, bg="gray", command=coord_get)
+    button.pack()
+
+    # メインループ
+    root.mainloop()
 
 def save_frame_camera_key(device_num, dir_path, basename,cycle, ext='jpg', delay=1, window_name='frame'):
+
     cap = cv2.VideoCapture(device_num)
     base_path="/home/user/Documents/data/"
     id = 35
@@ -16,15 +64,14 @@ def save_frame_camera_key(device_num, dir_path, basename,cycle, ext='jpg', delay
         ret, frame = cap.read()
         cv2.imshow(window_name, frame)
         key = cv2.waitKey(delay)
-        if  key == ord('q'):
-                break
+        if key == ord('q'):
+            break
         elif key == ord('c'):
-                strngr = "CYM"
-                print("座標は-->")
-                xy = input()
-                print("画像を保存します。ファイル名" + base_path + strngr + "_" + xy + "_"+str(id)+"." + ext)
-                cv2.imwrite('{}{}_{}_{}.{}'.format(base_path,strngr, xy, id, ext), frame)
-                id += 1
+            strngr = "CYM"
+            input_form()
+            print("画像を保存します。ファイル名" + base_path + strngr + "_" + coord + "_"+str(id)+"." + ext)
+            cv2.imwrite('{}{}_{}_{}.{}'.format(base_path,strngr, coord, id, ext), frame)
+            id += 1
     cv2.destroyWindow(window_name)
 
 #データの保存場所
