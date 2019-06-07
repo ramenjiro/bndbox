0a50f30f947cc2be356a6fcb757c48083ea581bb
kwhtsng <2018m002@onomichi-u.ac.jp>
2019-06-07 11:02:19 +0900

    とりあえず動いたコミット

---
+++ god_cap.py
@@ -0,0 +1,77 @@
+import cv2
+import os
+import numpy as np
+
+class PointList():
+    def __init__(self, npoints):
+        self.npoints = npoints
+        self.ptlist = np.empty((npoints, 2), dtype=int)
+        self.pos = 0
+
+    def add(self, x, y):
+        if self.pos < self.npoints:
+            self.ptlist[self.pos, :] = [x, y]
+            self.pos += 1
+            return True
+        return False
+
+
+def onMouse(event, x, y, flag, params):
+    wname, img, ptlist = params
+    if event == cv2.EVENT_MOUSEMOVE:  # マウスが移動したときにx線とy線を更新する
+        img2 = np.copy(img)
+        h, w = img2.shape[0], img2.shape[1]
+        cv2.line(img2, (x, 0), (x, h - 1), (255, 0, 0))
+        cv2.line(img2, (0, y), (w - 1, y), (255, 0, 0))
+        cv2.imshow(wname, img2)
+
+    if event == cv2.EVENT_LBUTTONDOWN:  # レフトボタンをクリックしたとき、ptlist配列にx,y座標を格納する
+        if ptlist.add(x, y):
+            print('[%d] ( %d, %d )' % (ptlist.pos - 1, x, y))
+            cv2.circle(img, (x, y), 3, (0, 0, 255), 3)
+            cv2.imshow(wname, img)
+        else:
+            print('All points have selected.  Press ESC-key.')
+        if(ptlist.pos == ptlist.npoints):
+            print(ptlist.ptlist)
+            cv2.line(img, (ptlist.ptlist[0][0], ptlist.ptlist[0][1]),
+                     (ptlist.ptlist[1][0], ptlist.ptlist[1][1]), (0, 255, 0), 3)
+            cv2.line(img, (ptlist.ptlist[1][0], ptlist.ptlist[1][1]),
+                     (ptlist.ptlist[2][0], ptlist.ptlist[2][1]), (0, 255, 0), 3)
+            cv2.line(img, (ptlist.ptlist[2][0], ptlist.ptlist[2][1]),
+                     (ptlist.ptlist[3][0], ptlist.ptlist[3][1]), (0, 255, 0), 3)
+            cv2.line(img, (ptlist.ptlist[3][0], ptlist.ptlist[3][1]),
+                     (ptlist.ptlist[0][0], ptlist.ptlist[0][1]), (0, 255, 0), 3)
+
+def save_frame_camera_key(device_num, dir_path, basename, ext='jpg', delay=1, window_name='frame'):
+    cap = cv2.VideoCapture(device_num)
+
+    if not cap.isOpened():
+        return
+
+    os.makedirs(dir_path, exist_ok=True)
+    base_path = os.path.join(dir_path, basename)
+
+    # n = 0
+    while True:
+        ret, frame = cap.read()
+        cv2.imshow(window_name, frame)
+        key = cv2.waitKey(delay) & 0xFF
+        if key == ord('c'):
+            img = frame
+            wname = "MouseEvent"
+            cv2.namedWindow(wname)
+            npoints = 4
+            ptlist = PointList(npoints)
+            cv2.setMouseCallback(wname, onMouse, [wname, img, ptlist])
+            cv2.imshow(wname, img)
+            cv2.waitKey()
+            cv2.destroyAllWindows()
+            # n += 1
+        elif key == ord('q'):
+            break
+
+    cv2.destroyWindow(window_name)
+
+
+save_frame_camera_key(0, 'data', 'camera_capture')
